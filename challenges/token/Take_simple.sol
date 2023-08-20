57d6cab7fc9693ed07acede8e8d3fc4181a09a08
brandonfrulla <bfrulla@mail.umw.edu>
2023-08-20 12:27:24 -0500

    fix

---
+++ challenges/token/Take_simple.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0; //lacks safe math
+
+import './Token.sol';
+
+contract Take {
+
+  address public taker;
+  Token public target;
+
+  constructor(address _tgt, address _taker) public {
+    target = Token(_tgt);
+    taker = _taker;
+  }
+
+  function transfer(uint _value) public {
+    target.transfer(taker, _value);
+  }
+}
---
+++ challenges/token/Token.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0; // no safe math in v 6
+
+contract Token {
+
+  mapping(address => uint) balances;
+  uint public totalSupply;
+
+  constructor(uint _initialSupply) public {
+    balances[msg.sender] = totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint _value) public returns (bool) {
+    require(balances[msg.sender] - _value >= 0); //if we call from another contract, balance[msg.sender] - _value will underflow 
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    return true;
+  }
+
+  function balanceOf(address _owner) public view returns (uint balance) {
+    return balances[_owner];
+  }
+}
